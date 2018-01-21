dd993dc1438a00a43660b7c0bd414fbaf5b7a9d9
ChildLee <471227142@qq.com>
2018-01-21 18:39:22 +0800

    slf4j

--- fast-mockmvc/src/test/java/com/fast/ApplicationTests.java
+++ fast-mockmvc/src/test/java/com/fast/ApplicationTests.java
@@ -1,8 +1,11 @@
 package com.fast;
 
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -21,6 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebAppConfiguration
 public class ApplicationTests {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     private MockMvc mockMvc;
 
     @Autowired
@@ -29,6 +34,7 @@ public class ApplicationTests {
     @Before
     public void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        log.error("初始化{}", "MockMvc");
     }
 
     @Test
