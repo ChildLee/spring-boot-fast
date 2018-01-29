4b575fad1e88888b6fe2b793953ddc1b939cb9da
ChildLee <471227142@qq.com>
2018-01-29 13:56:42 +0800

    security Filter

--- fast-security/src/main/java/com/fast/security/AuthenticationFilterConfig.java
+++ fast-security/src/main/java/com/fast/security/AuthenticationFilterConfig.java
@@ -1,11 +1,16 @@
 package com.fast.security;
 
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -13,30 +18,34 @@ import java.io.IOException;
 public class AuthenticationFilterConfig extends AbstractAuthenticationProcessingFilter {
     protected AuthenticationFilterConfig() {
         super(new AntPathRequestMatcher("/login", "POST"));
+        this.setAuthenticationFailureHandler(new SimpleUrlAuthenticationFailureHandler("/login?error"));
     }
-
-//    @Override
-//    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-//        HttpServletRequest request = (HttpServletRequest) req;
-//        HttpServletResponse response = (HttpServletResponse) res;
-//
-//        if (!requiresAuthentication(request, response)) {
-//            chain.doFilter(request, response);
-//            return;
-//        }
-//
-//        System.out.println("过滤器");
-//        String username = request.getParameter("username");
-//        String password = request.getParameter("password");
-//        System.out.println(username);
-//        System.out.println(password);
-//
-//        chain.doFilter(request, response);
-//    }
 
     private String usernameParameter = "username";
     private String passwordParameter = "password";
     private String validateParameter = "validate";
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        if (!requiresAuthentication(request, response)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        System.out.println("过滤器");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+        if (true) {
+            unsuccessfulAuthentication(request, response, new InsufficientAuthenticationException("验证码错误!"));
+            return;
+        }
+        chain.doFilter(request, response);
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
