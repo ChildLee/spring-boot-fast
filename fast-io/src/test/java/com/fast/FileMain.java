5f1cd8d3e67c75d45774029ff04dc2f436c657f2
ChildLee <471227142@qq.com>
2018-01-30 22:30:29 +0800

    IO

--- fast-io/src/test/java/com/fast/FileMain.java
+++ fast-io/src/test/java/com/fast/FileMain.java
@@ -6,33 +6,104 @@ import java.io.*;
 
 
 public class FileMain {
+
+    @Test
+    public void outputStream() throws IOException {
+        //写入数据
+        File file = new File("D:/a.txt");
+        String data = "我跟你说个锤子!";
+        //定义字节输出流
+        FileOutputStream fileOutputStream = new FileOutputStream(file, true);//true表示追加数据
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);//字节输出流缓存
+        bufferedOutputStream.write(data.getBytes());
+        bufferedOutputStream.flush();
+//        bufferedOutputStream.close();
+        //字节转字符
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
+        BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);//字符输入流缓存
+        bufferedWriter.write(data);
+        bufferedWriter.flush();
+        bufferedWriter.close();
+        //定义字符输出流
+        FileWriter fileWriter = new FileWriter(file, true);
+        BufferedWriter buffered = new BufferedWriter(fileWriter);//字符输入流缓存
+        buffered.write(data);
+        buffered.flush();
+        buffered.close();
+
+
+    }
+
     @Test
     public void inputStream() throws IOException {
-        File file = new File("D:/a.txt");
-        InputStream inputStream = new FileInputStream(file);
-        Reader inputStreamReader = new InputStreamReader(inputStream);
-        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while (null != (line = bufferedReader.readLine())) {
-            sb.append(line);
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < 99999; i++) {
+            //读取数据
+            File file = new File("D:/a.txt");
+            InputStream inputStream = new FileInputStream(file);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            byte[] bytes = new byte[8192];
+            int flag = 0;
+            StringBuilder sb = new StringBuilder();
+            while ((flag = bufferedInputStream.read(bytes)) != -1) {
+                sb.append(new String(bytes, 0, flag));
+            }
         }
-        bufferedReader.close();
-        System.out.println(sb);
+        System.out.println(System.currentTimeMillis() - a);
+//        System.out.println(sb);
+    }
+
+    @Test
+    public void inputStreamReader() throws IOException {
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < 99999; i++) {
+            File file = new File("D:/a.txt");
+            InputStream inputStream = new FileInputStream(file);
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while (null != (line = bufferedReader.readLine())) {
+                sb.append(line);
+            }
+            bufferedReader.close();
+        }
+        System.out.println(System.currentTimeMillis() - a);
+//        System.out.println(sb);
     }
 
     @Test
     public void fileReader() throws IOException {
-        //FileReader构造函数默认实例化FileInputStream字节流
-        FileReader fileReader = new FileReader("D:/a.txt");
-        BufferedReader reader = new BufferedReader(fileReader);
-        String line;
-        StringBuilder sb = new StringBuilder();
-        while (null != (line = reader.readLine())) {
-            sb.append(line);
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < 99999; i++) {
+            File file = new File("D:/a.txt");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while (null != (line = bufferedReader.readLine())) {
+                sb.append(line);
+            }
+            bufferedReader.close();
         }
-        reader.close();
-        System.out.println(sb);
+        System.out.println(System.currentTimeMillis() - a);
+//        System.out.println(sb);
+    }
+
+    @Test
+    public void byteArrayInputStream() throws IOException {
+        File file = new File("D:/a.txt");
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        byte[] bytes = new byte[8192];
+        int flag = 0;
+        StringBuilder sb = new StringBuilder();
+        while ((flag = fileInputStream.read(bytes)) != -1) {
+            outStream.write(bytes);
+        }
+        byte[] data = outStream.toByteArray(); // 取内存中保存的数据
+        String result = new String(data, "UTF-8");
+        System.out.println(result);
     }
 
     @Test
