0bc4fdfed134d58bb4f238e51626396c4bb96fea
ChildLee <471227142@qq.com>
2018-02-05 21:05:24 +0800

    Verification Code

--- fast-mockmvc/src/test/java/com/fast/ApplicationTests.java
+++ fast-mockmvc/src/test/java/com/fast/ApplicationTests.java
@@ -1,45 +1,33 @@
 package com.fast;
 
 
-import org.junit.Before;
+import com.fast.controller.AdminController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@WebAppConfiguration
+@WebMvcTest(AdminController.class)
 public class ApplicationTests {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext context;
-
-    @Before
-    public void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
-        log.error("初始化{}", "MockMvc");
-    }
+    private MockMvc mockMvc;
 
     @Test
     public void contextLoads() throws Exception {
-        String result = mockMvc.perform(get("/admin")
+        String result = mockMvc.perform(MockMvcRequestBuilders.get("/admin")
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("username", "username")
                 .param("password", "password"))
