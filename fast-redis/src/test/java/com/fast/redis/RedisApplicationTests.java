9bf473417b91367b51bca07f78c616dcc476e5b9
ChildLee <471227142@qq.com>
2018-02-06 21:34:44 +0800

    redis

--- fast-redis/src/test/java/com/fast/redis/RedisApplicationTests.java
+++ fast-redis/src/test/java/com/fast/redis/RedisApplicationTests.java
@@ -2,15 +2,23 @@ package com.fast.redis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     public void contextLoads() {
+        redisTemplate.opsForValue().set("换成", "撒个");
+        String redisValue = redisTemplate.opsForValue().get("1");
+        System.out.println(redisValue);
     }
 
 }
